538abb665a46a23c63f59cd98204dcb1554da9d9
Ravi Sharma <rsravisharma38@gmail.com>
2023-12-24 22:00:44 +0530

    images

--- resources/views/Frontend/app.blade.php
+++ resources/views/Frontend/app.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="images/header/favicon.png" type="image/x-icon" />      
     <title>@yield('title')</title>
 
     <!-- <link rel="stylesheet" href="{{ asset('build/assets/app-v1.0.0.css') }}">
