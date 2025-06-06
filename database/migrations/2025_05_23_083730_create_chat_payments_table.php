9e24e4c6eeafea9bdf4b93000c08c696083ee780
Ravi Sharma <rsravisharma38@gmail.com>
2025-06-06 13:49:56 +0530

    chat updated

--- database/migrations/2025_05_23_083730_create_chat_payments_table.php
+++ database/migrations/2025_05_23_083730_create_chat_payments_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chats_payments', function (Blueprint $table) {
+        Schema::create('chat_payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('chat_session_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chats_payments');
+        Schema::dropIfExists('chat_payments');
     }
 };
