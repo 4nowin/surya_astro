4d95654712f6d0de7951ea3069c119e720409f9a
Ravi Sharma <rsravisharma38@gmail.com>
2025-06-21 13:37:20 +0530

    making notifications

--- routes/web.php
+++ routes/web.php
@@ -196,4 +196,4 @@ Route::get('/test-env', function () {
     return env('FIREBASE_CREDENTIALS_PATH', 'not found');
 });
 
-Route::resource('/know-{id}', WebController::class, 'index')->where('id', '[^\-\/]+');
+Route::get('/know-{id}', WebController::class, 'index')->where('id', '[^\-\/]+');
